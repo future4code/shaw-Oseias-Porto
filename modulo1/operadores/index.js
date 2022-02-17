8643f53d54675cf8914269e0172bb59e8154144e
Oséias Porto <oseiasporto@msn.com>
2022-02-16 22:08:21 -0300

    exerciicos operadores incompleto

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,37 @@
+// ex 1
+
+const idadeUsuario = +prompt('qual a sua  idade?')
+const idadeAmigo = +prompt('qual a sua idade')
+console.log('sua idade é maior do que a do seu melhor amigo?', idadeUsuario > idadeAmigo)
+console.log(idadeUsuario + idadeAmigo)
+
+// ex 2
+
+const numeroPar = +prompt('insira número par')
+console.log(numeroPar % 2 )
+
+// qual quer que seja o numero par inserido o resto vai ser sempre zero
+
+// O rresultado vai ser sempre um.
+
+//ex 3
+
+const idadeEmAnos = +prompt('qual a sua idade em anos')
+
+console.log(idade)
+
+const idadeEmMeses = ('A sua idade em mesmes')
+
+console.log(idadeEmMeses)
+
+const idadeEmDias = ('A sua idade em dias')
+
+console.log('idadeEmDias')
+console.log('idadeEmHoras')
+
+// ex 4
+
+
+
+
+
